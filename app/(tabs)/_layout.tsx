d8b4a7254295b0c94f6f0168b4fdb95a6c562cdc
Jaclyn <z5463674@ad.unsw.edu.au>
2025-07-02 01:22:36 +1000

    fix conflict

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,57 +1,13 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-<<<<<<< HEAD
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-=======
 import { Platform, Text } from 'react-native';
 
 // 导入需要的组件和样式
 import { HapticTab } from '@/components/HapticTab';
->>>>>>> main
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-<<<<<<< HEAD
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
-=======
 const MockIconSymbol = ({ name, size, color } :any) => (
   <Text style={{ fontSize: size, color, pointerEvents: 'box-none' }}>
     {name}
@@ -153,4 +109,3 @@ const TabLayout = () => {
 };
 
 export default TabLayout;
->>>>>>> main
